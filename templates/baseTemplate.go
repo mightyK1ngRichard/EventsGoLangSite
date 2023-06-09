fd40f84299c27bc8383484c45910a8a4616dbb0c
Dmitiy <dimapermyakov55@gmail.com>
2023-06-09 04:06:36 +0300

    Update layout

--- templates/baseTemplate.go
+++ templates/baseTemplate.go
@@ -2,99 +2,29 @@ package templates
 
 func GetBaseHTML() string {
 	return `
-	<body class="d-flex flex-column h-100">
-<div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
-    <button class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center"
-            id="bd-theme"
-            type="button"
-            aria-expanded="false"
-            data-bs-toggle="dropdown"
-            aria-label="Toggle theme (auto)">
-        <svg class="bi my-1 theme-icon-active" width="1em" height="1em">
-            <use href="#circle-half"></use>
-        </svg>
-        <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
-    </button>
-    <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
-        <li>
-            <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light"
-                    aria-pressed="false">
-                <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em">
-                    <use href="#sun-fill"></use>
-                </svg>
-                Light
-                <svg class="bi ms-auto d-none" width="1em" height="1em">
-                    <use href="#check2"></use>
-                </svg>
-            </button>
-        </li>
-        <li>
-            <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark"
-                    aria-pressed="false">
-                <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em">
-                    <use href="#moon-stars-fill"></use>
-                </svg>
-                Dark
-                <svg class="bi ms-auto d-none" width="1em" height="1em">
-                    <use href="#check2"></use>
-                </svg>
-            </button>
-        </li>
-        <li>
-            <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto"
-                    aria-pressed="true">
-                <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em">
-                    <use href="#circle-half"></use>
-                </svg>
-                Auto
-                <svg class="bi ms-auto d-none" width="1em" height="1em">
-                    <use href="#check2"></use>
-                </svg>
-            </button>
-        </li>
-    </ul>
-</div>
-<header>
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Go Lang</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/events">Все мероприятия</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/tickets">Билеты</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/create-event">Создать меропрятие</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search" style="margin-right: 20px" method="POST" action="/events">
-                    <input name="searching_text" class="form-control me-2" type="search" placeholder="Поиск заголовков"
-                           aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Поиск</button>
-                </form>
-
-
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Регистрация</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Войти</a>
-                    </li>
-                </ul>
-
-            </div>
-        </div>
-    </nav>
-</header>
-</body>
+	<body>
+		<header class="p-3 text-bg-dark">
+			<div class="container">
+				<div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+		
+					<ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+						<li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+						<li><a href="/events" class="nav-link px-2 text-white">Мероприятия</a></li>
+						<li><a href="/tickets" class="nav-link px-2 text-white">Билеты</a></li>
+						<li><a href="/create-event" class="nav-link px-2 text-white">Создать</a></li>
+						<li><a href="#" class="nav-link px-2 text-white">Info</a></li>
+					</ul>
+		
+					<form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search" method="POST" action="/events">
+						<input name="searching_text" type="search" class="form-control form-control-dark text-bg-dark" placeholder="Поиск заголовка" aria-label="Search">
+					</form>
+						<div class="text-end">
+						<button type="button" class="btn btn-outline-light me-2">Войти</button>
+						<button type="button" class="btn btn-warning">Регистрация</button>
+					</div>
+				</div>
+			</div>
+		</header>
+	</body>
 	`
 }
